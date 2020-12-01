ab52fa2e880ce742b306c70d491007f7e320d14d
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-01 08:59:51 +0100

    Day 1 part 1

---
+++ day01/day01.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(module+ test
+  (require rackunit))
+
+(module+ main
+  (displayln "Day 1"))
+
+(define (read-input filename)
+  (with-input-from-file filename
+    (lambda ()
+      (for/list ([line (in-lines)])
+        (string->number line)))))
+
+(define (part1 filename)
+  (define expenses (read-input filename))
+  (for*/last ([a (in-list expenses)]
+              [b (in-list expenses)]
+              #:final (= 2020 (+ a b)))
+    (* a b)))
