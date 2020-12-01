382aa8a98c28eec86fd5c5c1ab49308711203e54
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-01 09:00:57 +0100

    Day 1 part 2

--- day01/day01.rkt
+++ day01/day01.rkt
@@ -18,3 +18,11 @@
               [b (in-list expenses)]
               #:final (= 2020 (+ a b)))
     (* a b)))
+
+(define (part2 filename)
+  (define expenses (read-input filename))
+  (for*/last ([a (in-list expenses)]
+              [b (in-list expenses)]
+              [c (in-list expenses)]
+              #:final (= 2020 (+ a b c)))
+    (* a b c)))
