68258767f71b551d1a4455c71e526c6f6c4e76fa
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-05 11:20:02 +0100

    Day 5 further improvement to binary decoding

--- day05/day05.dyalog
+++ day05/day05.dyalog
@@ -1,5 +1,4 @@
  ⎕IO←0
- p←⊃⎕NGET'input'1
- f←{+/8 1×2⊥¨'BR'⍷¨7((⊂↑),(⊂↓))⍵}
- ⌈/f¨p  ⍝ Part 1
- ⊢/((⍳⌈/)~⊢)f¨p  ⍝ Part 2
+ p←{2⊥⍵∊'BR'}¨⊃⎕NGET'input'1
+ ⌈/p  ⍝ Part 1
+ ⊢/(⍳⌈/p)~p  ⍝ Part 2
--- day05/day05.rkt
+++ day05/day05.rkt
@@ -6,43 +6,31 @@
 (module+ main
   (displayln "Day 5"))
 
-(define (parse-seat str)
-  (let* ([str (string-replace str "B" "1")]
-         [str (string-replace str "F" "0")]
-         [str (string-replace str "R" "1")]
-         [str (string-replace str "L" "0")]
-         [row (substring str 0 7)]
-         [col (substring str 7 10)])
-    (list (string->number row 2) (string->number col 2))))
+(define (seat-id str)
+  (define binary-str (list->string
+                      (for/list ([c (in-string str)])
+                        (if (or (equal? c #\B) (equal? c #\R))
+                            #\1
+                            #\0))))
+  (string->number binary-str 2))
 
 (define (read-input filename)
-  (define seats-str (file->lines filename))
-  (map parse-seat seats-str))
-
-(define (seat-id seat)
-  (+ (* 8 (car seat)) (cadr seat)))
+  (define seats (file->lines filename))
+  (map seat-id seats))
 
 (module+ test
-  (for ([seat-str '("FBFBBFFRLR" "BFFFBBFRRR" "FFFBBBFRRR" "BBFFBBFRLL")]
-        [expected-row '(44 70 14 102)]
-        [expected-col '(5 7 7 4)]
+  (for ([seat '("FBFBBFFRLR" "BFFFBBFRRR" "FFFBBBFRRR" "BBFFBBFRLL")]
         [expected-id '(357 567 119 820)])
-    (test-case seat-str
-      (let ([seat (parse-seat seat-str)])
-        (check-equal? (car seat)  expected-row)
-        (check-equal? (cadr seat) expected-col)
-        (check-equal? (seat-id seat) expected-id)))))
+    (check-equal? (seat-id seat) expected-id)))
 
 (define (part1 filename)
-  (define seats (read-input filename))
-  (apply max (map seat-id seats)))
+  (apply max (read-input filename)))
 
 (module+ main
   (displayln (part1 "input")))
 
 (define (part2 filename)
-  (define seats (read-input filename))
-  (define ids (map seat-id seats))
+  (define ids (read-input filename))
   (define id-range (range (apply min ids) (apply max ids)))
   (set-first (set-subtract (list->set id-range) (list->set ids))))
 
