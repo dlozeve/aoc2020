99a9d22a836c67b7240eb4c4b1be732c4c4c405f
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-08 09:54:24 +0100

    Day 8 part 2

--- day08/day08.rkt
+++ day08/day08.rkt
@@ -14,25 +14,57 @@
 (define (read-input filename)
   (list->vector (map parse-instruction (file->lines filename))))
 
-(define (run-until-loop program)
+(define (execute program)
   (define n (vector-length program))
   (let loop ([acc 0]
              [i 0]
              [visited '()])
-    (if (member i visited)
-        acc
-        (match (vector-ref program i)
-          [(list 'nop x) (loop acc (add1 i) (cons i visited))]
-          [(list 'acc x) (loop (+ acc x) (add1 i) (cons i visited))]
-          [(list 'jmp x) (loop acc (+ i x) (cons i visited))]
-          [instr (error "invalid instruction" i instr)]))))
+    (cond
+      [(member i visited) (values 'loop visited acc)]
+      [(= i n) (values 'end visited acc)]
+      [else (match (vector-ref program i)
+              [(list 'nop x) (loop acc (add1 i) (cons i visited))]
+              [(list 'acc x) (loop (+ acc x) (add1 i) (cons i visited))]
+              [(list 'jmp x) (loop acc (+ i x) (cons i visited))]
+              [instr (error "invalid instruction" i instr)])])))
 
 (define (part1 filename)
   (define program (read-input filename))
-  (run-until-loop program))
+  (define-values (state visited acc) (execute program))
+  acc)
 
 (module+ test
   (check-equal? (part1 "test") 5))
 
 (module+ main
   (displayln (part1 "input")))
+
+(define (change-instruction program visited)
+  (define new-program (vector-copy program))
+  (define new-visited
+    (let loop ([instructions visited])
+      (match (vector-ref new-program (car instructions))
+        [(list 'nop x)
+         (vector-set! new-program (car instructions) (list 'jmp x))
+         (cdr instructions)]
+        [(list 'jmp x)
+         (vector-set! new-program (car instructions) (list 'nop x))
+         (cdr instructions)]
+        [z (loop (cdr instructions))])))
+  (values new-program new-visited))
+
+(define (part2 filename)
+  (define program (read-input filename))
+  (define-values (initial-reason initial-visited initial-acc) (execute program))
+  (let loop ([visited initial-visited])
+    (let*-values ([(new-program new-visited) (change-instruction program visited)]
+                  [(reason visited acc) (execute new-program)])
+      (if (equal? reason 'end)
+          acc
+          (loop new-visited)))))
+
+(module+ test
+  (check-equal? (part2 "test") 8))
+
+(module+ main
+  (displayln (part2 "input")))
