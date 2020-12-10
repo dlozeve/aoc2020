3fc19125cc6986233822a72d283e2ed694df4ed2
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-10 14:02:19 +0100

    Day 10 part 2

--- day10/day10.rkt
+++ day10/day10.rkt
@@ -7,14 +7,14 @@
   (displayln "Day 10"))
 
 (define (read-input filename)
-  (map string->number (file->lines filename)))
+  (define input (map string->number (file->lines filename)))
+  (sort (cons 0 (cons (target input) input)) <))
 
-(define (device jolts)
-  (+ 3 (apply max jolts)))
+(define (target lst)
+  (+ 3 (apply max lst)))
 
 (define (part1 filename)
-  (define input (read-input filename))
-  (define jolts (sort (cons 0 (cons (device input) input)) <))
+  (define jolts (read-input filename))
   (define counts (make-hash))
   (for ([x (in-list (drop jolts 1))]
         [y (in-list jolts)])
@@ -27,3 +27,23 @@
 
 (module+ main
   (displayln (part1 "input")))
+
+(define (count-paths jolts)
+  (define counts (make-hash))
+  (hash-set! counts 0 1)
+  (for ([x (in-list (cdr jolts))])
+    (hash-set! counts x (+ (hash-ref counts (- x 1) 0)
+                           (hash-ref counts (- x 2) 0)
+                           (hash-ref counts (- x 3) 0))))
+  (hash-ref counts (last jolts)))
+
+(define (part2 filename)
+  (define jolts (read-input filename))
+  (count-paths jolts))
+
+(module+ test
+  (check-equal? (part2 "test1") 8)
+  (check-equal? (part2 "test2") 19208))
+
+(module+ main
+  (displayln (part2 "input")))
