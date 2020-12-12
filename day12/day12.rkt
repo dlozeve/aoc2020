ba355cb9d1420f25f79ac97b7cfbdf5470feed97
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-12 15:38:43 +0100

    Day 12 part 2

--- day12/day12.rkt
+++ day12/day12.rkt
@@ -40,3 +40,27 @@
 
 (module+ main
   (displayln (part1 "input")))
+
+(define (step2 st instr)
+  (match instr
+    [`(N ,n) (struct-copy state st [dir (+ (state-dir st) (make-rectangular 0 n))])]
+    [`(S ,n) (struct-copy state st [dir (- (state-dir st) (make-rectangular 0 n))])]
+    [`(E ,n) (struct-copy state st [dir (+ (state-dir st) (make-rectangular n 0))])]
+    [`(W ,n) (struct-copy state st [dir (- (state-dir st) (make-rectangular n 0))])]
+    [`(F ,n) (struct-copy state st [pos (+ (state-pos st) (* n (state-dir st)))])]
+    [`(R ,n) (struct-copy state st [dir (* (state-dir st) (make-polar 1 (degrees->radians (- n))))])]
+    [`(L ,n) (struct-copy state st [dir (* (state-dir st) (make-polar 1 (degrees->radians n)))])]))
+
+(define (part2 filename)
+  (define input (read-input filename))
+  (define final
+    (state-pos (for/fold ([st (state 0 (make-rectangular 10 1))])
+                         ([instr (in-list input)])
+                 (step2 st instr))))
+  (exact-round (+ (abs (real-part final)) (abs (imag-part final)))))
+
+(module+ test
+  (check-equal? (part2 "test") 286))
+
+(module+ main
+  (displayln (part2 "input")))
