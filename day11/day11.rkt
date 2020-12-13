212b65a429dbd22f21b51298e1c1dde627f5827e
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-13 16:46:57 +0100

    Day 11 part 2

--- day11/day11.rkt
+++ day11/day11.rkt
@@ -6,41 +6,114 @@
 (module+ main
   (displayln "Day 11"))
 
+(define (matrix-ref mat i j default)
+  (define n (vector-length mat))
+  (define m (vector-length (vector-ref mat 0)))
+  (if (or (< i 0) (<= n i) (< j 0) (<= m j))
+      default
+      (vector-ref (vector-ref mat i) j)))
+
+(define (matrix-set! mat i j v)
+  (vector-set! (vector-ref mat i) j v))
+
+(define (matrix-copy mat)
+  (for/vector ([l (in-vector mat)])
+    (vector-copy l)))
+
+(define (display-state state)
+  (for ([l (in-vector state)])
+    (displayln (list->string (vector->list l))))
+  (displayln ""))
+
+(define (parse-line str)
+  (list->vector (string->list str)))
+
 (define (read-input filename)
   (with-input-from-file filename
     (lambda ()
-      (for/hash ([line (in-lines)]
-                 [i (in-naturals)]
-                 #:when #t
-                 [c (in-list (string->list line))]
-                 [j (in-naturals)]
-                 #:when (equal? c #\L))
-        (values (list i j) 'empty)))))
+      (for/vector ([line (in-lines)])
+        (parse-line line)))))
 
-(define (update-seat state x y)
+(define (update-seat-neighbours state x y)
   (define neighbours (for*/sum ([i '(-1 0 1)]
                                 [j '(-1 0 1)]
                                 #:unless (= 0 i j))
-                       (if (eq? 'occupied (hash-ref state (list (+ x i) (+ y j)) 'floor)) 1 0)))
-  (define s (hash-ref state (list x y)))
+                       (if (eq? #\# (matrix-ref state (+ x i) (+ y j) #\.)) 1 0)))
+  (define s (matrix-ref state x y #\.))
   (cond
-    [(and (eq? s 'empty) (= neighbours 0)) 'occupied]
-    [(and (eq? s 'occupied) (>= neighbours 4)) 'empty]
+    [(and (eq? s #\L) (= neighbours 0)) #\#]
+    [(and (eq? s #\#) (>= neighbours 4)) #\L]
     [else s]))
+
+(define (update-state state update-seat-fn)
+  (define n (vector-length state))
+  (define m (vector-length (vector-ref state 0)))
+  (define new-state (matrix-copy state))
+  (for* ([x (in-range n)]
+         [y (in-range m)])
+    (matrix-set! new-state x y (update-seat-fn state x y)))
+  new-state)
+
+(define (count-occupied state)
+  (for*/sum ([l (in-vector state)]
+             [v (in-vector l)]
+             #:when (eq? v #\#))
+    1))
 
 (define (part1 filename)
   (define input (read-input filename))
+  (define n (vector-length input))
+  (define m (vector-length (vector-ref input 0)))
   (define-values (prev final)
-    (for/fold ([prev-state (hash)]
+    (for/fold ([prev-state 0]
                [state input])
               ([i (in-naturals)]
                #:break (equal? prev-state state))
-      (values state (for/hash ([(pos v) (in-hash state)])
-                      (values pos (update-seat state (car pos) (cadr pos)))))))
-  (length (filter (λ (s) (eq? s 'occupied)) (hash-values final))))
+      (define new-state (update-state state update-seat-neighbours))
+      (values state new-state)))
+  (count-occupied final))
 
 (module+ test
   (check-equal? (part1 "test") 37))
 
 (module+ main
   (displayln (part1 "input")))
+
+(define (visible state x y dx dy)
+  (define n (vector-length state))
+  (define m (vector-length (vector-ref state 0)))
+  (let loop ([u (+ x dx)]
+             [v (+ y dy)])
+    (cond
+      [(or (< u 0) (< v 0) (<= n u) (<= m v)) 0]
+      [(eq? #\L (matrix-ref state u v #\.)) 0]
+      [(eq? #\# (matrix-ref state u v #\.)) 1]
+      [else (loop (+ u dx) (+ v dy))])))
+
+(define (update-seat-visible state x y)
+  (define n-visible (apply + (for*/list ([dx '(-1 0 1)]
+                                         [dy '(-1 0 1)]
+                                         #:unless (= 0 dx dy))
+                               (visible state x y dx dy))))
+  (define s (matrix-ref state x y #\.))
+  (cond
+    [(and (eq? s #\L) (= n-visible 0)) #\#]
+    [(and (eq? s #\#) (>= n-visible 5)) #\L]
+    [else s]))
+
+(define (part2 filename)
+  (define input (read-input filename))
+  (define-values (prev final)
+    (for/fold ([prev-state 0]
+               [state input])
+              ([i (in-naturals)]
+               #:break (equal? prev-state state))
+      (define new-state (update-state state update-seat-visible))
+      (values state new-state)))
+  (count-occupied final))
+
+(module+ test
+  (check-equal? (part2 "test") 26))
+
+(module+ main
+  (displayln (part2 "input")))
