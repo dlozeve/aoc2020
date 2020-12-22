25ca29449cc25566a16994913b090c3a7f0107d6
Dimitri Lozeve <dimitri@lozeve.com>
2020-12-22 09:47:46 +0100

    Day 22 part 2

--- day22/day22.rkt
+++ day22/day22.rkt
@@ -10,23 +10,50 @@
 
 (define (play p1 p2)
   (match (list p1 p2)
-    [(list x '()) x]
-    [(list '() x) x]
-    [(list (list-rest a x) (list-rest b y))
-     (if (< a b)
-         (play x (append y (list b a)))
-         (play (append x (list a b)) y))]))
+    [(list xs '()) (list xs '())]
+    [(list '() ys) (list '() ys)]
+    [(list (list-rest x xs) (list-rest y ys))
+     (if (< x y)
+         (play xs (append ys (list y x)))
+         (play (append xs (list x y)) ys))]))
 
-(define (part1 filename)
-  (define-values (p1 p2)  (read-input filename))
-  (define final-deck (play p1 p2))
+(define (play-recursive deck1 deck2)
+  (define states (mutable-set))
+  (let loop ([p1 deck1]
+             [p2 deck2])
+    (let/cc return
+      (when (set-member? states (list p1 p2)) (return (list p1 '())))
+      (set-add! states (list p1 p2))
+      (match (list p1 p2)
+        [(list xs '()) (list xs '())]
+        [(list '() ys) (list '() ys)]
+        [(list (list-rest x xs) (list-rest y ys))
+         (if (and (<= x (length xs)) (<= y (length ys)))
+             (if (empty? (cadr (play-recursive (take xs x) (take ys y))))
+                 (loop (append xs (list x y)) ys)
+                 (loop xs (append ys (list y x))))
+             (if (> x y)
+                 (loop (append xs (list x y)) ys)
+                 (loop xs (append ys (list y x)))))]))))
+
+(define (winning-score play-fn deck1 deck2)
+  (define final-deck (apply append (play-fn deck1 deck2)))
   (for/sum ([x (in-list (reverse final-deck))]
             [i (in-naturals 1)])
     (* x i)))
 
+(define (part1 filename)
+  (define-values (deck1 deck2) (read-input filename))
+  (winning-score play deck1 deck2))
+
+(define (part2 filename)
+  (define-values (deck1 deck2) (read-input filename))
+  (winning-score play-recursive deck1 deck2))
+
 (module+ test
-  (check-equal? (part1 "test") 306))
+  (check-equal? (part1 "test") 306)
+  (check-equal? (part2 "test") 291))
 
 (module+ main
-  (displayln (part1 "input")))
-
+  (displayln (part1 "input"))
+  (displayln (part2 "input")))
